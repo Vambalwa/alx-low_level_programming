78dd3ecce460e3b8a693ed7cee236a2561019863
Vambalwa <velmavutagwa@gmail.com>
2022-09-08 13:57:50 -0700

    Putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+
+  * main - Entry point
+
+  *
+
+  * Return: Always 0 (Success)
+
+  */
 
 int main(void)
 
