13d945ed3d0d28a6ec8baa93a855dbe9e6720bf0
Vambalwa <velmavutagwa@gmail.com>
2022-09-13 12:27:07 -0700

    prints alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,12 @@
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	int n;
+
 	for (n = 'a'; n <= 'z'; n++)
 	{
 		_putchar(n);
