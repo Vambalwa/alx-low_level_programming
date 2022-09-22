7ea908cde8a4a7886400be8f1738b66d158dd725
Vambalwa <velmavutagwa@gmail.com>
2022-09-22 08:45:08 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,24 +10,24 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-		int a = 0, b = 0;
+	int a = 0, b = 0;
 
-		while (src[b])
-		{								
-			b++;
-		}
+	while (src[b])
+	{								
+		b++;
+	}
 
-		while (a < n && src[a])
-		{
-			dest[a] = src[a];
-			a++;
-		}
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
 
-		while (a < n)
-		{
+	while (a < n)
+	{
 		dest[a] = '\0';
 		a++;
-		}
+	}
 
-		return (dest);
+	return (dest);
 }
