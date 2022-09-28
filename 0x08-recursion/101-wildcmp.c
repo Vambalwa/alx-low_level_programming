2bd80180b01646ae8ba3801ac1b5f7164a01bbdd
Vambalwa <velmavutagwa@gmail.com>
2022-09-28 18:01:36 +0300

    compares 2 strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,6 +14,5 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
 		return (1);
 	if (*s2 == '*' && *(s1 + 1) && *s2)
-		return (0);
 	return (0);
 }
