d99dc434135b0de699d3389dcf5d70083f639865
Vambalwa <velmavutagwa@gmail.com>
2022-10-14 00:45:57 +0300

    prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
 
 /**
- *print_name - print the name
- *@name: string
- *@f: function to print with
- *
- *Return: Void
- */
-
+  * print_name - Prints a name
+  * @name: The name to prints
+  * @f: Pointer to function
+  *
+  * Return: Nothing
+  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != '\0' && f != '\0')
-f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
