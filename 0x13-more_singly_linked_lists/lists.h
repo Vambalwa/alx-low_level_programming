491edf6d64deae281eb5fa35a4b7f8caf731e23f
Vambalwa <velmavutagwa@gmail.com>
2022-10-26 23:53:17 +0300

    Header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef _S_LINK_LISTS_
-#define _S_LINK_LISTS_
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -27,5 +29,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
